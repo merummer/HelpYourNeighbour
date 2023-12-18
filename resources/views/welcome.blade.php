75961dd32bfce76cf1656b0c0ebcddab2c6840c8
Melissa Maria Rummer <mrummer@schueler.hakzell.at>
2023-12-18 13:02:53 +0100

    change welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>help your neighbour HYN</title>
+        <title>help your neighbour</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
