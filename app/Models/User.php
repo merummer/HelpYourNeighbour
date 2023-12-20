66861b62958260fc1f86e10c7fe8022d1a261783
Melissa Maria Rummer <mrummer@schueler.hakzell.at>
2023-12-20 14:41:52 +0100

    migration #8

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'skills'
     ];
+
+
 
     /**
      * The attributes that should be hidden for serialization.
