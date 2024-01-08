7cf3695e7b994ad41d1fc8e9d219ff940c544ebc
Michael Bacher <mbacher@schueler.hakzell.at>
2024-01-08 19:41:01 +0100

    Migration erstellt und Hilfsanfrage abschließen möglich

--- database/migrations/2023_12_20_130041_add_skills_to_users.php
+++ database/migrations/2023_12_20_130041_add_skills_to_users.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('helps', function (Blueprint $table) {
-            //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('skills')->default(' ');
         });
     }
 
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('helps', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
         });
     }
